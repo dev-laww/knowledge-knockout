8a34de27d07c39be04e30b8306be74ba899ecbc7
dev-laww <lawrenceallen.tech@gmail.com>
2024-05-18 22:02:20 +0800

    chore: Update file paths in database header and added docstrings

--- include/database.h
+++ include/database.h
@@ -1,8 +1,8 @@
 #ifndef DATABASE_H
 #define DATABASE_H
-#define LEADERBOARDS_FILE "leaderboards.txt"
-#define USERS_FILE "users.txt"
-#define TRIVIA_FILE "trivia.txt"
+#define LEADERBOARDS_FILE "data/leaderboards.txt"
+#define USERS_FILE "data/users.txt"
+#define TRIVIA_FILE "data/trivia.txt"
 
 #include <fstream>
 #include <vector>
@@ -41,12 +41,36 @@ namespace model
 
 namespace database
 {
+    /*
+     * @brief Function that returns the users from the database
+     * @return std::vector<model::User>
+     */
     std::vector<model::User> users();
+    /*
+     * @brief Function that returns the leaderboards from the database
+     * @return std::vector<model::Leaderboard>
+     */
     std::vector<model::Leaderboard> leaderboards();
+    /*
+     * @brief Function that returns the trivias from the database
+     * @return std::vector<model::Trivia>
+     */
     std::vector<model::Trivia> trivias();
 
+    /*
+     * @brief Function that saves the users to the database
+     * @param users: vector of users
+     */
     void save_users(const std::vector<model::User> &users);
+    /*
+     * @brief Function that saves the leaderboards to the database
+     * @param leaderboards: vector of leaderboards
+     */
     void save_leaderboards(const std::vector<model::Leaderboard> &leaderboards);
+    /*
+     * @brief Function that saves the trivias to the database
+     * @param trivias: vector of trivias
+     */
     void save_trivias(const std::vector<model::Trivia> &trivias);
 };
 
